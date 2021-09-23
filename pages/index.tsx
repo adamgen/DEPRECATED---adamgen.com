9d70109eb4aeddd1e87b64c9490790b776eb2908
Adam <adamgensh@gmail.com>
2021-09-23 23:08:27 +0300

    Add svg wave to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
+
+import Svg from './wave.svg';
 
 const Home: NextPage = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Wave />
+        </div>
+    );
 };
 
 export default Home;
+
+const Wave = styled(Svg)`
+    color: #000;
+`;
